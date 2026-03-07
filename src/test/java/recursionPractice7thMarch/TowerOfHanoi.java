9045515dbfcda60825981a095d133c3d336422a1
Nitya <nityapractice@gmail.com>
2026-03-07 21:06:55 +0530

    test: added commented in tower of hanoi code

--- src/test/java/recursionPractice7thMarch/TowerOfHanoi.java
+++ src/test/java/recursionPractice7thMarch/TowerOfHanoi.java
@@ -3,13 +3,19 @@ package recursionPractice7thMarch;
 public class TowerOfHanoi {
 	
 	public static void solve(int n, String src, String helper, String dest) {
+		//base Condition
 		if(n == 1) {
 			System.out.println("Pick "+n+ " from "+ src +" and put it on " + dest);
 			return;
 		}
 		
+		//pick n-1 and put it on helper
 		solve(n-1, src, dest, helper);
+		
+		//Pick last disk and put it on Dest
 		System.out.println("Pick "+n+ " from "+ src +" and put it on " + dest);
+		
+		//pick disk from helper and put it on dest
 		solve(n-1, helper, src, dest);
 	}
 
