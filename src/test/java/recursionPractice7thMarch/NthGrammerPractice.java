8850b91f9895664f1c154ede5e22a0393b9058d6
Nitya Ranjan <nityapractice@gmail.com>
2026-03-07 22:38:19 +0530

    Feature (#1)
    * test: nth grammer practice is pushed
    
    * test: added commented in tower of hanoi code
    
    * test: added nth grammer comments

--- src/test/java/recursionPractice7thMarch/NthGrammerPractice.java
+++ src/test/java/recursionPractice7thMarch/NthGrammerPractice.java
@@ -1,21 +1,18 @@
 package recursionPractice7thMarch;
 
-public class NthGrammer {
+public class NthGrammerPractice {
 	
 	public static int solve(int n, int k) {
-		//base condition
 		if(n ==1 && k ==1) {
 			return 0;
 		}
 		
-		//identify the mid
-		int mid = (int) (Math.pow(2, n-1)/2);
+		int length = (int) Math.pow(2, n-1);
+		int mid = length/2;
 		
-		//if k is less than equal to mid then solve it with n-1 and k
 		if(k<=mid) {
 			return solve(n-1, k);
 		}else {
-			//otherwise solve it with n-1 and k-mid with its complement
 			return 1 ^ solve(n-1, k-mid);
 		}
 	}
@@ -23,7 +20,6 @@ public class NthGrammer {
 	public static void main(String[] args) {
 		
 		
-		System.out.println(solve(2, 2));
 
 	}
 
