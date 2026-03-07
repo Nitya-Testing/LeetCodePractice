fe84c37995de810a16b1858590997785d0e3c367
Nitya <nityapractice@gmail.com>
2026-03-07 21:09:04 +0530

    test: added nth grammer comments

--- src/test/java/recursionPractice7thMarch/NthGrammer.java
+++ src/test/java/recursionPractice7thMarch/NthGrammer.java
@@ -3,15 +3,19 @@ package recursionPractice7thMarch;
 public class NthGrammer {
 	
 	public static int solve(int n, int k) {
+		//base condition
 		if(n ==1 && k ==1) {
 			return 0;
 		}
 		
+		//identify the mid
 		int mid = (int) (Math.pow(2, n-1)/2);
 		
+		//if k is less than equal to mid then solve it with n-1 and k
 		if(k<=mid) {
 			return solve(n-1, k);
 		}else {
+			//otherwise solve it with n-1 and k-mid with its complement
 			return 1 ^ solve(n-1, k-mid);
 		}
 	}
